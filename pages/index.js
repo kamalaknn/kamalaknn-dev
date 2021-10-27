28aaa2011eae1f3a348ebc5c69212b158723fb5b
Kamalakannan <nkamalakannan1992@gmail.com>
2021-10-27 21:25:32 +0530

    Update top level domain

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>kamalaknn.com</title>
+        <title>kamalaknn.dev</title>
         <meta name="description" content="More updates coming soon..." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
