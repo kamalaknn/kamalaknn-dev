950a2f1653065e2cee2b607663d9e59730b90401
Kamalakannan <nkamalakannan1992@gmail.com>
2021-10-31 15:16:38 +0530

    Added basic top bar with navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'media', // 'false' or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    mode: "jit",
+    purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    darkMode: 'media', // 'false' or 'media' or 'class'
+    theme: {
+        extend: {},
+        container: {
+            center: true
+        }
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
